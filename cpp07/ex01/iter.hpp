db264fb04b7b218276c2d32887c5c2ca947822b3
goblgobl01 <maarafimohammed@gmail.com>
2026-05-19 14:20:31 +0100

    alo

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 
-template <typename T1, typename T2, typename T3>void iter(T1 &a,const T2 &b, T3 c)
+template <typename T1, typename T2, typename T3>void iter(T1 &a,const T2 &b, T3 func)
 {
 	T2 i;
 
 	i = 0;
 	while(i < b)
 	{
-		c(a[i]);
+		func(a[i]);
 		i++;
 	}
 }
